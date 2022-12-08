8f7fedbec99ff3aa9b23338565f82203d91e0ca6
Brian Lambert <brianlambert2408@gmail.com>
2022-12-08 11:25:56 +1100

    Completed tasks

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,16 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    render json: Student.find(params[:id])
+  end
+
+  def grades
+
+  end
+
+  def highest_grade
+
+  end
+
 end
